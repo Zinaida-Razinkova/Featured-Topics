a38cd546d569bbe4b10897d39621b6fde69662f3
Zinaida Razinkova <razinkova.zina@gmail.com>
2024-09-20 17:30:01 +0300

    delete js

--- js/scroll.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', function () {
-    this.scrollY > 0 ? this.document.querySelector('.header').classList.add('scroll') : this.document.querySelector('.header').classList.remove('scroll')
-
-})
